58cc35cf74d5a3ef57c03fbff94a3dadb1b9c259
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-07-11 22:18:29 +0700

    readline standard lib nodejs

--- general.mjs
+++ general.mjs
@@ -5,10 +5,26 @@
 // setTimeout(sayHelloTimeOut, 2000);
 
 // ( process )
-import process from "process";
+// import process from "process";
 
-const exitFunction = () => {
-  console.info("Execution NodeJS was Succes");
+// const exitFunction = () => {
+//   console.info("Execution NodeJS was Succes");
+// };
+
+// process.addListener("exit", exitFunction);
+
+// ( readline )
+import process from "process";
+import readline from "readline";
+
+const input = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const yourName = (name) => {
+  console.info(`Hello ${name}`);
+  input.close();
 };
 
-process.addListener("exit", exitFunction);
+input.question("Enter your name ", yourName);
