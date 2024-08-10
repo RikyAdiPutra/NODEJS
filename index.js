d76b99621708ef0b6a40ec596207079923d32634
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-08-10 10:54:53 +0700

    iife in nodejs

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-const add = require("./add");
-
-console.log("Halo Riky");
-const sum = add(1, 2);
-const sum2 = add(1, 4);
-console.log(sum);
-console.log(sum2);
+require("./superman");
+require("./batman");
