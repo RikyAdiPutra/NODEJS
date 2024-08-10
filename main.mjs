ae7673bc700a829a3d5940ddb3157b5e2774b517
Riky Adi Putra <rikyadiputra76@gmail.com>
2024-08-11 06:47:01 +0700

    esm nodejs

---
+++ main.mjs
@@ -0,0 +1,6 @@
+import { add, substract } from "./math-esm.mjs";
+
+// const { add, substract } = math;
+
+console.log(add(5, 5));
+console.log(substract(5, 5));
---
+++ math-esm.mjs
@@ -0,0 +1,9 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const substract = (a, b) => {
+  return a - b;
+};
+
+// export default { add, substract };
